2b7e8562db960f869c7001d0956a46d90b51257f
Vansh Sharma <vanshsharma9354@gmail.com>
2022-02-07 18:30:19 +0530

    Update: Add the Portfolio link

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,15 +3,21 @@ const Links = (props) => {
   return (
     <>
       <main className={styles.links}>
-        <a className={styles.code} href="https://github.com/VanshSh/know-emoticon_neogcamp">
+        <a
+          className={styles.code}
+          href='https://github.com/VanshSh/know-emoticon_neogcamp'
+        >
           GitHub Code
         </a>
-        <a className={styles.portfolio} href="#">
+        <a
+          className={styles.portfolio}
+          href='https://vanshsharma.netlify.app/index.html'
+        >
           Portfolio
         </a>
       </main>
     </>
-  );
+  )
 };
 
 export default Links;
